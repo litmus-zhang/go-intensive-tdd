8c7d6e710e516dfc63dc57a2e7f8ee031e507af0
litmus-zhang <abdulsalamlukmon9@gmail.com>
2024-05-13 12:24:09 +0100

    feat: Add SumAll and SumAllTail functions
    The code changes in this commit add two new functions, SumAll and SumAllTail, to the `arrayslices` package in the `sum.go` file.
    
    The SumAll function takes multiple slices of integers as input and returns a slice containing the sum of each input slice. It uses a for loop to iterate over the input slices and calls the existing Sum function to calculate the sum for each slice.
    
    The SumAllTail function is similar to SumAll, but it calculates the sum of all elements except the first element in each input slice. It uses slicing to create a new slice excluding the first element and then calls the Sum function to calculate the sum.
    
    Corresponding test functions, `TestSumAll` and `TestSumAllTails`, are added to the `sum_test.go` file to verify the correctness of the SumAll and SumAllTail functions respectively. These test functions create input slices and call the respective functions, then compare the returned sums with the expected sums and report errors if they don't match.
    
    These additions provide additional functionality to calculate the sum of multiple slices of numbers in Go programs.

--- go-basics/array_slices/sum_test.go
+++ go-basics/array_slices/sum_test.go
@@ -1,6 +1,9 @@
 package arrayslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5}
@@ -9,5 +12,22 @@ func TestSum(t *testing.T) {
 
 	if got != expect {
 		t.Errorf("got %d expected %d given %v", got, expect, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	expect := []int{3, 9}
+	if !reflect.DeepEqual(got, expect) {
+		t.Errorf("got %v expected %v", got, expect)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTail([]int{1, 2}, []int{0, 9})
+	expect := []int{2, 9}
+
+	if !reflect.DeepEqual(got, expect) {
+		t.Errorf("got %v expected %v", got, expect)
 	}
 }
